2383fbc61654ad203a1b09fd56e3ef58791169ff
Tetiana Susol <tetyanamyeskalo@gmail.com>
2025-09-05 13:25:34 +0300

    Commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,4 +4,89 @@
 // const Game = require('../modules/Game.class');
 // const game = new Game();
 
-// Write your code here
+const Game = require('../modules/Game.class');
+const game = new Game();
+
+// елементи DOM
+const cells = Array.from(document.querySelectorAll('.field-cell'));
+const scoreElement = document.querySelector('.game-score');
+const startButton = document.querySelector('.button');
+const messageStart = document.querySelector('.message-start');
+const messageWin = document.querySelector('.message-win');
+const messageLose = document.querySelector('.message-lose');
+
+// відмалювати поле
+function renderBoard() {
+  const board = game.getState().flat();
+
+  cells.forEach((cell, i) => {
+    const value = board[i];
+
+    cell.textContent = value === 0 ? '' : value;
+    cell.className = 'field-cell'; // скидаємо стилі
+
+    if (value > 0) {
+      cell.classList.add(`field-cell--${value}`);
+    }
+  });
+  scoreElement.textContent = game.getScore();
+  renderStatus();
+}
+
+function renderStatus() {
+  messageStart.classList.add('hidden');
+  messageWin.classList.add('hidden');
+  messageLose.classList.add('hidden');
+
+  if (game.getStatus() === 'win') {
+    messageWin.classList.remove('hidden');
+  } else if (game.getStatus() === 'lose') {
+    messageLose.classList.remove('hidden');
+  }
+}
+
+// запуск гри
+function startGame() {
+  game.start();
+  renderBoard();
+  startButton.textContent = 'Restart';
+  startButton.classList.remove('start');
+  startButton.classList.add('restart');
+}
+
+function restartGame() {
+  game.restart();
+  renderBoard();
+}
+
+startButton.addEventListener('click', () => {
+  if (game.getStatus() === 'idle') {
+    startGame();
+  } else {
+    restartGame();
+  }
+});
+
+// обробка клавіш
+document.addEventListener('keydown', (e) => {
+  let moved = false;
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      moved = game.moveLeft();
+      break;
+    case 'ArrowRight':
+      moved = game.moveRight();
+      break;
+    case 'ArrowUp':
+      moved = game.moveUp();
+      break;
+    case 'ArrowDown':
+      moved = game.moveDown();
+      break;
+  }
+
+  if (moved) {
+    renderBoard();
+  }
+});
